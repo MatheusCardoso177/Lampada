f18baaea6c10c230b2eac2edb686a11538d431e6
Matheus Cardoso Pereira <76125566+MatheusCardoso177@users.noreply.github.com>
2022-12-22 13:52:05 -0300

    Add files via upload

---
+++ Lampada/lampada.js
@@ -0,0 +1,47 @@
+const turnOn = document.getElementById ('turnOn');
+const turnOff = document.getElementById ('turnOff');
+
+const lamp= document.getElementById('lamp')
+
+
+
+function isLampBroken() {
+    return lamp.src.indexOf ('quebrada') > -1
+}
+
+
+
+
+
+
+function lampOn() {
+    lamp.src= './img/ligada.jpg';
+}
+
+function lampOff () {
+    lamp.scr= "/img/desligada.jpg"
+}
+
+
+function lampBroken() {
+    lamp.src = '/img/quebrada.jpg'
+}
+
+
+
+
+
+
+
+turnOn.addEventListener('click', lampOn);
+
+turnOff.addEventListener('click', lampOff);
+
+
+lamp.addEventListener ('mouseover', lampOn);
+lamp.addEventListener ('mouseleave', lampOff);
+lampOff.addEventListener ('dblclick', lampBroken)
+
+
+
+
